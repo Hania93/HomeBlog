47100c5d8d6ff84ef2c52fcd9eab59a50f23cc4e
Hania <hannanawrot.93@gmail.com>
2025-11-20 17:44:16 +0100

    add routes

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact.ejs");
+});
+
+app.get("/editPost", (req, res) => {
+  res.render("pages/editPost.ejs");
+});
+
+app.get("/showPost", (req, res) => {
+  res.render("pages/showPost.ejs");
 });
 
 app.listen(port, () => {
-    console.log(`server running on ${port}`);
+  console.log(`server running on ${port}`);
 });
